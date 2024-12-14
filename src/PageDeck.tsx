a8c9752e1af0426ed17f80908aff384e3638af20
LevoraShelby <levorashelby@gmail.com>
2024-12-14 04:32:22 -0600

    Very basic keybindings modal added.

--- src/PageDeck.tsx
+++ src/PageDeck.tsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 import Homepage from './Homepage.tsx'
 import WhoAmI from './WhoAmI.tsx'
+import KeybindingsModal from './KeybindingsModal.tsx'
 import './PageDeck.css'
 
 function PageDeck() {
 	const [position, setPosition] = useState(1)
 	// TODO: Move this into a Context when we start using more keybinds
+	const [showKeybindingsModal, setShowKeybindingsModal] = useState(false)
 	const [keyBuffer, setKeyBuffer] = useState<string[]>([])
 
 	const onKeyPress = (event: React.KeyboardEvent<any>) => {
 		if (keyBuffer.length == 0 && event.key === "p") {
 			setKeyBuffer(["p"])
+			return
+		}
+		if (keyBuffer.length == 0 && event.key === "?") {
+			setShowKeybindingsModal((val) => !val)
+			return
+		}
+		if (keyBuffer.length == 0 && event.key === "Escape" && showKeybindingsModal) {
+			setShowKeybindingsModal(false)
 			return
 		}
 		if (keyBuffer.length == 0) {
@@ -25,6 +35,8 @@ function PageDeck() {
 	return (<div onKeyUp={onKeyPress} tabIndex={0} className='pageDeck'>
 		{ position == 1 && <Homepage/> }
 		{ position == 2 && <WhoAmI/> }
+		{ /*TODO: KeybindingsModal doesn't always toggle if it was toggled just a second ago*/ }
+		<KeybindingsModal isOpen={showKeybindingsModal} />
 	</div>)
 }
 
