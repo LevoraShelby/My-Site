00f76143199cf9934a53e5597b604badb0f3afc7
LevoraShelby <levorashelby@gmail.com>
2024-12-14 03:13:11 -0600

    Prettied things up

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -2,7 +2,7 @@ function Homepage() {
 	return (<>
 		<h1>Levora's Website</h1>
 		<p>~Welcome~</p>
-		<p>This website is different from most others. I have taken away your cursor. Today you will be honorable and navigate only using your keyboard. Press '?' for a list of all keybindings.</p>
+		<p>This website is different from most others. I have taken away your cursor. Today you will be honorable and navigate using only your keyboard. Press '<b>?</b>' for a list of all keybindings.</p>
 	</>)
 }
 
--- src/WhoAmI.tsx
+++ src/WhoAmI.tsx
@@ -1,20 +1,23 @@
 import "./WhoAmI.css" 
 
 function WhoAmI() {
-	return (<div className="whoami">
-			<h1>Who Am I?</h1>
-			<h3>Who Am I?</h3>
-			<p>My name is Levora, and my pronouns are she/her!</p>
-			<h3>What do I do?</h3>
-			<p>I'm a programmer and sometimes a writer. I also like playing TTRPGs and I am working on creating a GURPS module where the players fight dinosaurs with futuristic weaponry.</p>
-			<h3>What's my favorite bird?</h3>
-			<p>I think cardinals are pretty cool. Look at this one:</p>
-			<img src="https://www.scenichudson.org/wp-content/uploads/2022/11/scenichudson_20220138.jpg" />
-			<p className="caption">Frank Beres</p>
-			<h3>What do I make this site with?</h3>
-			<p>I made this site as a react app with Vite as a starting point. I'm also working on it all in VIM, my beloved.</p>
-			<img src="https://media1.tenor.com/m/xl-PA-sk1LcAAAAd/vim-my-beloved.gif" className="beloved" title="https://tenor.com/view/vim-my-beloved-vi-unix-command-line-gif-24684584" />
-			<br/>
+	// TODO: Add dive after h1 to keep content from going on the sides
+	return (<div className="whoamibg">
+			<div className="whoami">
+				<h1>Who Am I?</h1>
+				<h2>Who Am I?</h2>
+				<p>My name is Levora, and my pronouns are she/her!</p>
+				<h2>What do I do?</h2>
+				<p>I'm a programmer and sometimes a writer. I also like playing TTRPGs and I am working on creating a GURPS module where the players fight dinosaurs with futuristic weaponry.</p>
+				<h2>What's my favorite bird?</h2>
+				<p>I think cardinals are pretty cool. Look at this one:</p>
+				<img src="https://www.scenichudson.org/wp-content/uploads/2022/11/scenichudson_20220138.jpg" />
+				<p className="caption">Frank Beres</p>
+				<h2>What do I make this site with?</h2>
+				<p>I made this site as a react app with Vite as a starting point. I'm also working on it all in VIM, my beloved.</p>
+				<img src="https://media1.tenor.com/m/xl-PA-sk1LcAAAAd/vim-my-beloved.gif" className="beloved" title="https://tenor.com/view/vim-my-beloved-vi-unix-command-line-gif-24684584" />
+				<br/>
+			</div>
 		</div>)
 }
 
