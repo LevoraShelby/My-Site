d989289bb039276cf1be8b58e0f719e68bb32459
LevoraShelby <levorashelby@gmail.com>
2024-12-18 04:58:10 -0600

    Kicked up the state into a new top-level App component.
    This should prove useful once we add new pages that rely keybinds. This
    way there will be a single source of truth for the key buffer.

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import {createContext, ReactElement, useReducer, useState} from "react";
+import PageDeck from "./PageDeck";
+
+const KeyBufferContext = createContext<string[]>([])
+const SetKeyBufferContext = createContext<(s: string[])=>void>((s)=>{})
+
+function App(): ReactElement {
+	const [keyBuffer, setKeyBuffer] = useState<string[]>([])
+	return (<>
+			<KeyBufferContext.Provider value={keyBuffer}>
+				<SetKeyBufferContext.Provider value={setKeyBuffer}>
+					<PageDeck/>
+				</SetKeyBufferContext.Provider>
+			</KeyBufferContext.Provider>
+		</>)
+}
+
+export default App
+export {KeyBufferContext,SetKeyBufferContext}
