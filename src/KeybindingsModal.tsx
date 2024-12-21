89877f5957f133a75a92ce022e23bab644c2f211
LevoraShelby <levorashelby@gmail.com>
2024-12-21 07:44:57 -0600

    Prettying

--- src/KeybindingsModal.tsx
+++ src/KeybindingsModal.tsx
@@ -1,12 +1,15 @@
+import {ReactElement} from 'react'
 import Modal from 'react-modal'
 
 // TODO: Some sort of warning is appearing whenever this opens and closes. Needs to be fixed
-const KeybindingsModal: React.FC<{isOpen?: boolean}> = ({isOpen}) => {
-	return (<Modal isOpen={isOpen ?? false} contentLabel='Keybindings'>
-		<p>p,1 =&gt; Home Page</p>
-		<p>p,2 =&gt; Who Am I?</p>
-		<p>? =&gt; Toggle keybindings modal</p>
-	</Modal>)
+// TODO: Add new keybindings
+// TODO: Use keyboard icons when making this pretty
+const KeybindingsModal = (props: {isOpen: boolean}): ReactElement => {
+	return (<Modal isOpen={props.isOpen ?? false} contentLabel='Keybindings'>
+			<p>p,1 =&gt; Home Page</p>
+			<p>p,2 =&gt; Who Am I?</p>
+			<p>? =&gt; Toggle keybindings modal</p>
+		</Modal>)
 }
 
 export default KeybindingsModal
